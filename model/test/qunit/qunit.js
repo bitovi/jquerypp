3e2c03046ca2047a1e7b65fbec308955264b44b7
Brian Moschel <brian.moschel@gmail.com>
2012-01-25 02:30:43 -0500

    accidentally commented out some tests

--- model/test/qunit/qunit.js
+++ model/test/qunit/qunit.js
@@ -2,8 +2,8 @@
 steal("jquery/model","jquery/dom/fixture")  //load your app
  .then('funcunit/qunit')  //load qunit
  .then("./model_test.js")//,"./associations_test.js")
- // .then(
-	// "jquery/model/backup/qunit",
-	// "jquery/model/list/list_test.js"
-  // )
-  // .then("jquery/model/validations/qunit/validations_test.js")
+ .then(
+	"jquery/model/backup/qunit",
+	"jquery/model/list/list_test.js"
+  )
+  .then("jquery/model/validations/qunit/validations_test.js")
