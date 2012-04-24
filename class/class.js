76077eda35cae65f8f028917d9e60593d7d4c794
David Luecke <daff@neyeon.com>
2012-04-24 16:26:29 -0600

    Some compatibility mappings

--- class/class.js
+++ class/class.js
@@ -7,5 +7,11 @@ steal("can/construct/proxy","can/construct/super",function( $ ) {
 
 	$.Class = can.Construct;
 
-
+	var old = $.Class.extend;
+	$.Class.extend = function() {
+		var cls = old.apply(this, arguments);
+		cls.prototype.Class = cls.prototype.constructor;
+		cls.prototype.callback = cls.prototype.proxy;
+		return cls;
+	}
 })();
--- controller/controller.js
+++ controller/controller.js
@@ -4,4 +4,7 @@ steal('jquery/class','can/control/plugin',function( $ ) {
 	$.fn.controller = $.fn.control;
 	$.fn.controllers = $.fn.controllers;
 
+	$.Controller.prototype.find = function(selector) {
+		return this.element.find(selector);
+	}
 });
