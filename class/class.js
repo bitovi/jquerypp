a6090022d1d50952d6a35e22105ec1060e685926
David Luecke <daff@neyeon.com>
2012-04-30 12:46:42 -0600

    Removing mappings

--- class/class.js
+++ class/class.js
@@ -4,13 +4,5 @@
 // It provides class level inheritance and callbacks.
 //!steal-clean
 steal("can/construct/proxy","can/construct/super",function( $ ) {
-	var old = can.Construct.extend;
-	can.Construct.extend = function() {
-		var cls = old.apply(this, arguments);
-		cls.prototype.Class = cls.prototype.constructor;
-		cls.prototype.callback = cls.prototype.proxy;
-		return cls;
-	}
-
 	$.Class = can.Construct;
 })();
--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,4 @@
 steal('jquery/class','can/control/plugin',function( $ ) {
-	can.Control.prototype.bind = can.Control.prototype.on;
-	can.Control.prototype.find = function(selector) {
-		return this.element.find(selector);
-	}
-
 	$.Controller = can.Control;
 	$.fn.controller = $.fn.control;
 	$.fn.controllers = $.fn.controllers;
