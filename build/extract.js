d5d0d229c808530313ed0afddedaf165a2a082b6
David Luecke <daff@neyeon.com>
2012-05-30 14:49:22 -0600

    Plugin extract script, udpated build

---
+++ build/extract.js
@@ -0,0 +1,50 @@
+steal('steal/build/pluginify', function(steal) {
+	var inexcludes = function(excludes, src) {
+			for(var i = 0; i < excludes.length; i++) {
+				if(src.indexOf(excludes[i]) !== -1) {
+					return true;
+				}
+			}
+			return false;
+		},
+		getDependencies = function(file, options, callback) {
+			steal.build.open("steal/rhino/empty.html", {
+				startFile : file,
+				skipCallbacks: true
+			}, function(opener){
+				var ret = [];
+				opener.each(function(stl, text){
+					if(!inexcludes(options.exclude || [], stl.rootSrc)) {
+						ret.push(stl);
+					}
+				});
+				callback(ret);
+			}, null);
+		};
+
+	steal.build.extract = function(plugins, options) {
+		var ops = steal.extend({ global : 'jQuery' }, options);
+		print('Extracting plugin files');
+		for(var file in plugins) {
+			var content = "/** \n" +
+				" * " + plugins[file] + "\n * \n";
+
+			getDependencies(file, ops, function(steals) {
+				if(steals.length > 1) {
+					content += " * Dependencies:\n * \n";
+				}
+				steals.forEach(function(stl) {
+					if(stl.rootSrc !== file) {
+						content += " * - " + (plugins[stl.rootSrc] || stl.rootSrc) + "\n";
+					}
+				});
+				if(steals.length > 1) {
+					content += " * \n";
+				}
+				content += " */\n" + steal.build.pluginify.content({ rootSrc : file }, ops);
+				new steal.File(options.out + plugins[file]).save(content);
+				print('  > ' + file + ' -> ' + plugins[file]);
+			});
+		}
+	}
+});
