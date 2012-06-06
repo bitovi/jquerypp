b3e4e5c14e330feb5a34cf3f73b7b19072eb0859
Justin Meyer <justinbmeyer@gmail.com>
2012-06-06 00:25:48 -0500

    removing analytics

--- javascripts/application.js
+++ javascripts/application.js
@@ -326,13 +326,3 @@ if($('.lt-ie7, .lt-ie8, .lt-ie9').length == 0){
 
 $('a[href$=#dom_helpers], a[href$=#events]').prepend('<span class="special">&#x2605;</span>');
 
-// google analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-2302003-11']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
