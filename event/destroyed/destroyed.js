c43893a8ea3c6a875c066c9c38bcaeb8084ff857
David Luecke <daff@neyeon.com>
2012-06-07 09:57:37 -0600

    Annotated destroyed

--- event/destroyed/destroyed.js
+++ event/destroyed/destroyed.js
@@ -4,14 +4,18 @@ steal('jquery/event').then(function( $ ) {
 	 * @download  http://jmvcsite.heroku.com/pluginify?plugins[]=jquery/dom/destroyed/destroyed.js
 	 * @test jquery/event/destroyed/qunit.html
 	 */
+
+	// Store the old jQuery.cleanData
 	var oldClean = jQuery.cleanData;
 
+	// Overwrites cleanData which is called by jQuery on manipulation methods
 	$.cleanData = function( elems ) {
 		for ( var i = 0, elem;
 		(elem = elems[i]) !== undefined; i++ ) {
+			// Trigger the destroyed event
 			$(elem).triggerHandler("destroyed");
-			//$.event.remove( elem, 'destroyed' );
 		}
+		// Call the old jQuery.cleanData
 		oldClean(elems);
 	};
 
