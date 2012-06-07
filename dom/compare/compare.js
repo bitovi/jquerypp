f3995829dcb11e3994d098c8f330b2d92818589c
David Luecke <daff@neyeon.com>
2012-06-07 09:57:15 -0600

    Annotated compare

--- dom/compare/compare.js
+++ dom/compare/compare.js
@@ -22,42 +22,50 @@ steal('jquery/dom').then(function($){
  * @param {HTMLElement|jQuery} element an element or jQuery collection to compare against.
  * @return {Number} A number representing a bitmask deatiling how the elements are positioned from each other.
  */
+
+// See http://ejohn.org/blog/comparing-document-position/
 jQuery.fn.compare = function(element){ //usually 
-	//element is usually a relatedTarget, but element/c it is we have to avoid a few FF errors
-	
-	try{ //FF3 freaks out with XUL
+	try{
+		// Firefox 3 throws an error with XUL - we can't use compare then
 		element = element.jquery ? element[0] : element;
 	}catch(e){
 		return null;
 	}
-	if (window.HTMLElement) { //make sure we aren't coming from XUL element
 
+	// make sure we aren't coming from XUL element
+	if (window.HTMLElement) {
 		var s = HTMLElement.prototype.toString.call(element)
 		if (s == '[xpconnect wrapped native prototype]' || s == '[object XULElement]' || s === '[object Window]') {
 			return null;
 		}
-
 	}
+
 	if(this[0].compareDocumentPosition){
+		// For browsers that support it, use compareDocumentPosition
+		// https://developer.mozilla.org/en/DOM/Node.compareDocumentPosition
 		return this[0].compareDocumentPosition(element);
 	}
+
+	// this[0] contains element
 	if(this[0] == document && element != document) return 8;
-	var number = (this[0] !== element && this[0].contains(element) && 16) + (this[0] != element && element.contains(this[0]) && 8),
+
+	var number =
+			// this[0] contains element
+			(this[0] !== element && this[0].contains(element) && 16) +
+			// element contains this[0]
+			(this[0] != element && element.contains(this[0]) && 8),
 		docEl = document.documentElement;
+
+	// Use the sourceIndex
 	if(this[0].sourceIndex){
+		// this[0] precedes element
 		number += (this[0].sourceIndex < element.sourceIndex && 4)
+		// element precedes foo[0]
 		number += (this[0].sourceIndex > element.sourceIndex && 2)
+		// The nodes are in different documents
 		number += (this[0].ownerDocument !== element.ownerDocument ||
 			(this[0] != docEl && this[0].sourceIndex <= 0 ) ||
 			(element != docEl && element.sourceIndex <= 0 )) && 1
-	}else{
-		var range = document.createRange(), 
-			sourceRange = document.createRange(),
-			compare;
-		range.selectNode(this[0]);
-		sourceRange.selectNode(element);
-		compare = range.compareBoundaryPoints(Range.START_TO_START, sourceRange);
-		
 	}
 
 	return number;
