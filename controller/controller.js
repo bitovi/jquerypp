a3c0dcd37bc2192bcaf61a9a1f226fb3d5eb967e
Noah Sloan <nsloan@spredfast.com>
2012-08-26 19:47:56 -0500

    controls, not controllers

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,7 @@
 steal('jquery', 'can/control', 'jquery/class','can/control/plugin',function($, Control) {
 	$.Controller = Control;
 	$.fn.controller = $.fn.control;
-	$.fn.controllers = $.fn.controllers;
+	$.fn.controllers = $.fn.controls;
 	can.Control.prototype.callback = can.Control.prototype.proxy;
 	$.Controller.init = function() {
 		this.prototype.Class = this;
