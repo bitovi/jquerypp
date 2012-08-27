96be3c6f534b5e2e09ee81d44cbc885b7f9e15a3
Noah Sloan <nsloan@spredfast.com>
2012-08-26 19:48:45 -0500

    Restore Controller.find,bind & delegate

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,12 @@ steal('jquery', 'can/control', 'jquery/class','can/control/plugin',function($, C
 	$.Controller = Control;
 	$.fn.controller = $.fn.control;
 	$.fn.controllers = $.fn.controls;
+	can.Control.prototype.find = can.Control.prototype.find || function(s) {
+		return this.element.find(s);
+	};
 	can.Control.prototype.callback = can.Control.prototype.proxy;
+	$.Controller.prototype.bind = $.Controller.prototype.on;
+	$.Controller.prototype.delegate = $.Controller.prototype.on;
 	$.Controller.init = function() {
 		this.prototype.Class = this;
 	};
