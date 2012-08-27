2739ca877403996a11b8933367197734967cc7b0
Noah Sloan <nsloan@spredfast.com>
2012-08-26 19:49:37 -0500

    restore Model getters

--- model/model.js
+++ model/model.js
@@ -6,6 +6,11 @@ steal('jquery', 'can/util', 'can/model','can/observe/attributes','can/observe/se
 	can.Model.callback = can.Construct.prototype.proxy;
 
 	$.Model.prototype.attrs = $.Model.prototype.attr;
+	var get = $.Model.prototype.__get;
+	$.Model.prototype.__get = function(attr) {
+		var getter = attr && ("get" + can.classize(attr));
+		return typeof this[getter] === 'function' ? this[getter]() : get.apply(this,arguments);
+	};
 	$.Model.init = function() {
 		this.prototype.Class = this;
 		this.callback = can.Construct.prototype.proxy;
