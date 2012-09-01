e5d311c15c9dd0f94430dac6daad669e21887236
Noah Sloan <nsloan@spredfast.com>
2012-08-31 22:51:02 -0500

    restore list.get(model(s))

--- model/model.js
+++ model/model.js
@@ -7,4 +7,19 @@ steal('jquery', 'can/util', 'can/model','can/observe/attributes','can/observe/se
 		var getter = attr && ("get" + can.classize(attr));
 		return typeof this[getter] === 'function' ? this[getter]() : get.apply(this,arguments);
 	};
+	// List.get used to take a model or list of models
+	var getList = $.Model.List.prototype.get;
+	$.Model.List.prototype.get = function(arg) {
+		var ids;
+		if(arg instanceof $.Model.List) {
+			ids = [];
+			$.each(arg,function() {
+				ids.push(this.attr('id'));
+			});
+			arg = ids;
+		} else if(arg.attr && arg.attr('id')) {
+			arg = arg.attr('id');
+		}
+		return getList.apply(this,arguments);
+	};
 });
