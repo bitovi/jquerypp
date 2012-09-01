4aba7e974c2175cece9fae3a9740d657bb919f70
Noah Sloan <nsloan@spredfast.com>
2012-08-31 22:47:14 -0500

    controls not controllers

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,5 @@
 steal('jquery', 'can/control', 'jquery/class','can/control/plugin',function($, Control) {
 	$.Controller = Control;
 	$.fn.controller = $.fn.control;
-	$.fn.controllers = $.fn.controllers;
+	$.fn.controllers = $.fn.controls;
 });
