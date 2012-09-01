fabf75cbc958a464ae9898d32544eaedbac830bf
Noah Sloan <nsloan@spredfast.com>
2012-08-31 22:50:37 -0500

    restore model getters

--- model/model.js
+++ model/model.js
@@ -2,4 +2,9 @@
 
 steal('jquery', 'can/util', 'can/model','can/observe/attributes','can/observe/setter','can/model/elements', function($, can){
 	$.Model = can.Model;
+	var get = $.Model.prototype.__get;
+	$.Model.prototype.__get = function(attr) {
+		var getter = attr && ("get" + can.classize(attr));
+		return typeof this[getter] === 'function' ? this[getter]() : get.apply(this,arguments);
+	};
 });
