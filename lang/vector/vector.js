f56463eb013c385c3c76dadf22231488d8ba229f
David Luecke <daff@neyeon.com>
2012-11-02 13:09:26 -0600

    Check for changedTouches. Should close #52

--- lang/vector/vector.js
+++ lang/vector/vector.js
@@ -180,9 +180,9 @@ steal('jquery', function($){
 	};
 
 	$.Event.prototype.vector = function() {
-		var
-			// Get the first touch element for touch events
-			touches = "ontouchend" in document && this.originalEvent.touches.length ? this.originalEvent.changedTouches[0] : this;
+		// Get the first touch element for touch events
+		var touches = "ontouchend" in document && this.originalEvent.touches && this.originalEvent.touches.length
+				? this.originalEvent.changedTouches[0] : this;
 		if ( this.originalEvent.synthetic ) {
 			var doc = document.documentElement,
 				body = document.body;
