d0712618d55398205f9ee8a0ef75eb1f02ccbb35
Farhana Basrawala (fara82) <fbasrawala@gmail.com>
2013-05-26 17:14:36 -0400

    fix tests for swipe for various browsers esp IE9

--- event/swipe/swipe_test.js
+++ event/swipe/swipe_test.js
@@ -1,30 +1,49 @@
 steal('funcunit/qunit', 'funcunit/syn', 'jquerypp/event/swipe', function (QUnit, Syn) {
+	var swipe;
 
-	module("jquerypp/swipe", {setup : function () {
-		$("#qunit-test-area").html("")
-		var div = $("<div id='outer'>" +
-			"<div id='inner1'>one</div>" +
-			"<div id='inner2'>two<div id='inner3'>three</div></div>" +
-			"</div>");
+	module("jquerypp/swipe", {
+		setup : function () {
+			$("#qunit-test-area").html("")
+			var div = $("<div id='outer'>" +
+				"<div id='inner1'>one</div>" +
+				"<div id='inner2'>two<div id='inner3'>three</div></div>" +
+				"</div>");
 
-		div.appendTo($("#qunit-test-area"));
-		var basicCss = {
-			position : "absolute",
-			border : "solid 1px black"
-		}
-		$("#outer").css(basicCss).css({top : "10px", left : "10px",
-			zIndex : 1000, backgroundColor : "green", width : "200px", height : "200px"})
-	}});
+			div.appendTo($("#qunit-test-area"));
+			var basicCss = {
+				position : "absolute",
+				border : "solid 1px black"
+			}
+			$("#outer").css(basicCss).css({top : "10px", left : "10px",
+				zIndex : 1000, backgroundColor : "green", width : "200px", height : "200px"})
 
-	test("swipe right event", 2, function () {
 
-		$("#outer").bind("swipe",function () {
-			ok(true, "swipe called");
-		}).bind("swipeleft",function () {
-				ok(false, "swipe left")
-			}).bind("swiperight", function () {
-				ok(true, "swiperight")
+			swipe = {};
+
+			$("#outer").bind("swipe",function () {
+				console.log('swipe')
+				swipe.general = true;
+			})
+			.bind("swipeleft",function () {
+				console.log('left')
+				swipe.left = true;
+			})
+			.bind("swiperight", function () {
+				console.log('right')
+				swipe.right = true;
+			})
+			.bind("swipeup", function () {
+				console.log('up')
+				swipe.up = true;
+			})
+			.bind("swipedown", function () {
+				console.log('down')
+				swipe.down = true;
 			});
+		}
+	});
+
+	test("swipe right event", 5, function () {
 		stop();
 		Syn.drag({
 			from : "20x20",
@@ -32,20 +51,20 @@ steal('funcunit/qunit', 'funcunit/syn', 'jquerypp/event/swipe', function (QUnit,
 			duration : 100
 		}, "outer", function () {
 			start();
+
+			ok(swipe.general, 'swipe');
+			ok(swipe.right, 'swipe right');
+
+			ok(!swipe.left, 'swipe left');
+			ok(!swipe.up, 'swipe up');
+			ok(!swipe.down, 'swipe down');
 		})
 
 	});
 
 
-	test("swipe left event", 2, function () {
-		var swipe = {};
 
-		$("#outer").bind("swipe",function () {
-			swipe.general = true;
-		}).bind("swipeleft",function () {
-			swipe.left = true;
-		});
-
+	test("swipe left event", 5, function () {
 		stop();
 
 		Syn.drag({
@@ -54,23 +73,18 @@ steal('funcunit/qunit', 'funcunit/syn', 'jquerypp/event/swipe', function (QUnit,
 			duration : 100
 		}, "outer", function () {
 			start();
-			ok(swipe.general, 'swipe')
-			ok(swipe.left, "swipe left")
+
+			ok(swipe.general, 'swipe');
+			ok(swipe.left, 'swipe left');
+
+			ok(!swipe.right, 'swipe right');
+			ok(!swipe.up, 'swipe up');
+			ok(!swipe.down, 'swipe down');
 		})
 	});
 
 
-	test("swipe up event", 2, function () {
-
-		$("#outer").bind("swipe",function () {
-			ok(true, "swipe called");
-		}).bind("swipeup",function () {
-				ok(true, "swipe left")
-			}).bind("swiperight",function () {
-				ok(false, "swiperight")
-			}).bind("swipedown", function () {
-				ok(false, "swipedown")
-			});
+	test("swipe up event", 5, function () {
 		stop();
 		Syn.drag({
 			from : "20x50",
@@ -78,21 +92,18 @@ steal('funcunit/qunit', 'funcunit/syn', 'jquerypp/event/swipe', function (QUnit,
 			duration : 100
 		}, "outer", function () {
 			start();
+
+			ok(swipe.general, 'swipe');
+			ok(swipe.up, 'swipe up');
+
+			ok(!swipe.left, 'swipe left');
+			ok(!swipe.right, 'swipe right');
+			ok(!swipe.down, 'swipe down');
 		})
 
 	});
 
-	test("swipe down event", 2, function () {
-
-		$("#outer").bind("swipe",function () {
-			ok(true, "swipe called");
-		}).bind("swipeup",function () {
-				ok(false, "swipe left")
-			}).bind("swiperight",function () {
-				ok(false, "swiperight")
-			}).bind("swipedown", function () {
-				ok(true, "swipedown")
-			});
+	test("swipe down event", 5, function () {
 		stop();
 		Syn.drag({
 			from : "20x20",
@@ -100,6 +111,12 @@ steal('funcunit/qunit', 'funcunit/syn', 'jquerypp/event/swipe', function (QUnit,
 			duration : 100
 		}, "outer", function () {
 			start();
+			ok(swipe.general, 'swipe');
+			ok(swipe.down, 'swipe down');
+
+			ok(!swipe.left, 'swipe left');
+			ok(!swipe.right, 'swipe right');
+			ok(!swipe.up, 'swipe up');
 		})
 
 	});
